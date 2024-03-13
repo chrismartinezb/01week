dd73d22b93a107e0a8bfd533fe9501d8c74961c4
Chris Martínez <chrismartinez@Chriss-MacBook-Air.local>
2024-03-13 11:43:29 +0100

    pipeline for csv files

--- docker/ingest_data.py
+++ docker/ingest_data.py
@@ -15,15 +15,26 @@ def main(params):
     port = params.port
     db = params.db
     table_name = params.table_name
-    url = params.url
-    parquet_name = 'output.parquet'
+    file_url = params.url
+    filename = 'output_file'  # A generic name for the downloaded file
+
+    # Determine file extension and set the appropriate pandas read function
+    file_ext = file_url.split('.')[-1].lower()
+    if file_ext in ['parquet']:
+        read_func = pd.read_parquet
+        filename += '.parquet'
+    elif file_ext in ['csv']:
+        read_func = pd.read_csv
+        filename += '.csv'
+    else:
+        raise ValueError(f"Unsupported file format: {file_ext}")
 
     print('Initialize data download from source')
-    os.system(f'wget {url} -O {parquet_name}')
+    os.system(f'wget {file_url} -O {filename}')
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df = pd.read_parquet(parquet_name)
+    df = read_func(filename)
 
     print('Schema created')
     df.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
